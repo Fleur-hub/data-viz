59e2e8f0c3461be865ff57c08d3cf254c13efe3c
Fleur-hub <elodie20.roussel@sfr.fr>
2021-04-08 18:21:45 +0200

    feat(front) partially added random circles graph

--- sketch.js
+++ sketch.js
@@ -29,9 +29,9 @@ function drawGlobal(){
 	let x = (screen.width)/2 - ((table.getRowCount()/2) * (space_between_chart) - (space_between_chart - rect_width)/2);
 	var i = 0;
 	for(; i < table.getRowCount(); ++i){
-		fill("#FFD95C");
-		rect(x + space_between_chart*i, y + 15, rect_width, (table.getRow(i).get('male_duration'))/10 - 50);
 		fill("#25696B");
+		rect(x + space_between_chart*i, y + 15, rect_width, (table.getRow(i).get('male_duration'))/10 - 50);
+		fill("#FFD95C");
 		rect(x + space_between_chart*i, y, rect_width, -(table.getRow(i).get('female_duration'))/10 + 50);
 	}
 
@@ -46,9 +46,9 @@ function drawChart(table, year){
     var i = 0;
     for(; i < 12; ++i){
     	console.log("here");
-        fill("#FFD95C");
+    	fill("#25696B");
         rect(x + space_between_chart*i, y + 15, rect_width, (table.getRow(i + (year * 12)).get('male_duration'))/10 - 50);
-        fill("#25696B");
+        fill("#FFD95C");
         rect(x + space_between_chart*i, y, rect_width, -(table.getRow(i + (year * 12)).get('female_duration'))/10 + 50);
     }
 
@@ -63,9 +63,9 @@ function drawDotGraph(table, year){
     var i = 0;
     for(; i < 12; ++i){
     	console.log("here");
-        fill("#FFD95C");
+    	fill("#25696B");
         rect(x + space_between_chart*i, y + 15, rect_width, (table.getRow(i + (year * 12)).get('male_duration'))/10 - 50);
-        fill("#25696B");
+        fill("#FFD95C");
         rect(x + space_between_chart*i, y, rect_width, -(table.getRow(i + (year * 12)).get('female_duration'))/10 + 50);
     }
 
@@ -82,9 +82,9 @@ function sideLegends(x, y, size, dateFirst, dateLast){
 }
 
 function legends(x, y){
-    fill("#25696B");
+	fill("#FFD95C");
     square(x, y, 20);
-    fill("#FFD95C");
+    fill("#25696B");
     square(x, y + 30, 20);
     fill("#313033")
     textSize(15);
@@ -106,8 +106,66 @@ function draw() {
 	}
 	else{
 		console.log(channel);
-		drawChart(table, 0);
+		hello();
+		//drawChart(table, 0);
 	}
     noLoop();
+}
+
+// follows this tutorial:
+// https://www.youtube.com/watch?v=XATr_jdh-44
+
+// Uses P5.js for canvas creation and drawing
+function drawMonthGraph() {
+  var circles = [],
+      circle = {},
+      overlapping = false,
+      NumCircles = 24,
+      protection = 500,
+      counter = 0,
+
+  //var canvasMonth = createCanvas(screenWidth, 400);
+  //canvasMonth.position(windowWidth, 500);
+
+  // populate circles array
+  // brute force method continues until # of circles target is reached
+  // or until the protection value is reached
+  while (circles.length < NumCircles && counter < protection) {
+    circle = {
+      x: random(width),
+      y: random(height)?
+      r: random(3, 36)
+    };
+    overlapping = false;
+
+    // check that it is not overlapping with any existing circle
+    // another brute force approach
+    for (var i = 0; i < circles.length; i++) {
+      var existing = circles[i];
+      var d = dist(circle.x, circle.y, existing.x, existing.y)
+      if (d < circle.r + existing.r) {
+        // They are overlapping
+        overlapping = true;
+        // do not add to array
+        break;
+      }
+    }
+
+    // add valid circles to array
+    if (!overlapping) {
+      circles.push(circle);
+    }
+
+    counter++;
+  }
+
+  // circles array is complete
+  // draw canvas once
+  background("#233")
+  fill("#2AC1A6");
+  noStroke();
+  for (var i = 0; i < circles.length; i++) {
+    ellipse(circles[i].x, circles[i].y, circles[i].r*2, circles[i].r*2);
+  }
 }
 
